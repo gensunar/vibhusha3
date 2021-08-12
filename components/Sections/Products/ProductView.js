5a1ba398987d078407497885ef4f0f073527ad03
Rahul Sunar <rahulsunar46@gmail.com>
2021-08-12 20:13:37 +0530

    correct

--- components/Sections/Products/ProductView.js
+++ components/Sections/Products/ProductView.js
@@ -7,7 +7,15 @@ export default function ProductView(){
     const [products, setProducts] = useState([])
     
     const fetchData = async () => {
-        const res = await fetch(`${baseurl}/products/get-products`)
+        const res = await fetch(`${baseurl}/products/get-products`,
+        {
+            method: "GET",
+            headers: {
+                "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36",
+                Accept: "application/json; charset=UTF-8",
+            },
+
+        })
         const data = await res.json()
         console.log(data)
 
