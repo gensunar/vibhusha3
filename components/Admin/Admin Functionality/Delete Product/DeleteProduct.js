959c6561bff2877c566a93e9b8d051c19325e9f5
Rahul Sunar <rahulsunar46@gmail.com>
2022-02-01 17:34:21 +0530

    New

--- components/Admin/Admin Functionality/Delete Product/DeleteProduct.js
+++ components/Admin/Admin Functionality/Delete Product/DeleteProduct.js
@@ -7,27 +7,7 @@ export default function DeleteProduct(props) {
                 <ProductMap />
             <h2>Deleting Product</h2>
             <div className={styles.main_container}>
-                <h2 onClick={fetchData}>View all Product</h2>
-                <div className={styles.content}>
-                    {product.map((item) => {
-
-                        return (
-                            <>
-                            <div className={styles.product_container} key={item.productId}>
-                                <div className={styles.text_container}>
-
-                                    <h1>{item.productId}</h1>
-                                    <h1>{item.productName}</h1>
-                                    <h2>{item.price}</h2>
-                                    <p>{item.productImage}</p>
-                                    <h2>{item.productDescription}</h2>
-                                </div>
-                                <button className={styles.delete}>Delete</button>
-                            </div>
-                           </>
-                        )
-                    })}
-                </div>
+               
             </div>
         </>
     )
