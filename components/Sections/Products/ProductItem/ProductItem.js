3f84a0e84400585b7327b623ddabedb0ee44b050
Rahul Sunar <rahulsunar46@gmail.com>
2022-02-08 22:14:30 +0530

    product page updated

--- components/Sections/Products/ProductItem/ProductItem.js
+++ components/Sections/Products/ProductItem/ProductItem.js
@@ -1,27 +1,35 @@
-import styles from './ProductItem.module.css'
-import Link from 'next/link'
-import router, {useRouter} from 'next/router'
-
+import styles from "./ProductItem.module.css";
+import Link from "next/link";
+import router, { useRouter } from "next/router";
 
 export default function ProductItem(props) {
-    const productRoute = () => {
-        router.push(`/products/${props.id}`)
-    }   
-    return (
-        <>
-            <div className={styles.card} onClick={productRoute}>
-                <div className={styles.image_container} 
-                  style= {{ backgroundImage: `url(${props.image})` }}
-                >
-                    {/* <Image className={styles.image} src={props.image} alt={props.title} layout="fill" /> */}
-                </div>
-                <div className={styles.text_container}>
-                    <div className={styles.title}>{props.title}</div>
-                    <div className={styles.text}>
-                        Rs. {props.price}
-                    </div>
-                </div>
-            </div>
-        </>
-    )
+  const productRoute = () => {
+    router.push(`/products/${props.id}`);
+  };
+
+  //Calculating the discount value
+  const value = 5666-props.price
+  const discount = Math.round(value/5666*100)
+
+  return (
+    <>
+      <div className={styles.card} onClick={productRoute}>
+        <div
+          className={styles.image_container}
+          style={{ backgroundImage: `url(${props.image})` }}
+        >
+          {/* <Image className={styles.image} src={props.image} alt={props.title} layout="fill" /> */}
+        </div>
+        <div className={styles.text_container}>
+          <span className={styles.title}>{props.title}</span>
+          <span className={styles.description}>{props.description}</span>
+          <div className={styles.price}>
+            <span className={styles.discount_price}>&#x20B9; 5666</span>
+            <span className={styles.show_price}>&#x20B9; {props.price}</span>
+            <span className={styles.discount}>({discount}% off)</span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
